a7bf00f9a16b5eb3ff9bb6bfa4393d326ae614cd
AnnaSinyaya <aasinyaya@gmail.com>
2022-01-06 18:07:41 +0300

    homework #1

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="example">
+            <input type="text" v-bind:value="name" v-on:input="name = $event.target.value">
+            <hr>
+            <h2>Hello, {{ name }}!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'InputComponent',
+  data () {
+    return {
+      name: 'Vue.js'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.example {
+  width: 70%;
+  margin: 20px auto;
+}
+
+h2 {
+  font-weight: normal;
+  font-size:28px;
+  color: #42b983;
+}
+</style>
